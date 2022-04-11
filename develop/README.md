377535ec4603ec37effc12137645d3da730d310d
TrevorTress <trevor.tress@outlook.com>
2022-04-10 23:26:57 -0400

    full readme generates

---
+++ develop/README.md
@@ -0,0 +1,43 @@
+
+  # Title
+
+  Description
+
+  ## License
+
+  [undefined](MIT)
+
+  ## Installation
+  
+  Instructions
+
+  ## Usage
+
+  Usage
+
+  ## Contributing
+
+  Contribution
+
+  ## Testing
+
+  Testing
+
+  ## Github Profile
+
+  Github
+  [Link to Profile](Profile Link)
+
+  ## Contact
+
+  You can reach me with further questions at [my email](email).
+
+  ## Table of Contents
+
+  1. [Title](#title)
+  2. [License](#license)
+  3. [Installation](#install)
+  4. [Usage](#usage)
+  5. [Constributing](#contri)
+  6. [Testing](#test)
+  7. [Profile](#profile)
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -35,5 +35,43 @@ module.exports = mdData => {
 
   ${mdData.description}
 
-`;
+  ## License
+
+  [${mdData.badge}](${mdData.license})
+
+  ## Installation
+  
+  ${mdData.install}
+
+  ## Usage
+
+  ${mdData.usage}
+
+  ## Contributing
+
+  ${mdData.contribution}
+
+  ## Testing
+
+  ${mdData.test}
+
+  ## Github Profile
+
+  ${mdData.user}
+  [Link to Profile](${mdData.profileLink})
+
+  ## Contact
+
+  You can reach me with further questions at [my email](${mdData.email}).
+
+  ## Table of Contents
+
+  1. [Title](#title)
+  2. [License](#license)
+  3. [Installation](#install)
+  4. [Usage](#usage)
+  5. [Constributing](#contri)
+  6. [Testing](#test)
+  7. [Profile](#profile)
+`
 }
