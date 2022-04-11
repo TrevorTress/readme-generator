9d3371e391df3c264fa7c0a89e2cf9ad23293d50
TrevorTress <trevor.tress@outlook.com>
2022-04-10 23:21:50 -0400

    functional generation

--- develop/index.js
+++ develop/index.js
@@ -1,57 +1,99 @@
-// TODO: Include packages needed for this application
+// packages
 const inquirer = require("inquirer");
 const fs = require('fs')
 const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
+// question array
 const questions = 
 [
-{
-    type: 'input',
-    name: 'name',
-    message: 'What is your name? (Required)' 
-},
-
-{
-    type: 'input',
-    name: 'activity',
-    message: 'What are you up to? (Required)' 
-}   
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true
+            } else {
+                console.log('Please enter your Project Title!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: "PLease enter your project's installation instructions."
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter your projects usage information'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: "Please enter your project's contribution guidelines"
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "Please enter your project's test instructions"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose a license for your project",
+        choices: ["MIT", "BSD",  "GPL"]
+    },
+    {
+        type: "input",
+        name: "link",
+        message: "Please add a link to your project's license"
+    },
+    {
+        type: "input",
+        name: "user",
+        message: "Please enter your github username."
+    },
+    {
+        type: "input",
+        name: "profileLink",
+        message: "Please enter a link to your Github profile"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address"
+    }   
 ];
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// initializes app
-const init = mdData => {
+// prompts the user with the questions array and returns the data as promise
+const promptUser = mdData => {
     if(!mdData) {
         mdData = []
     }
-    // prompts the user with the questions array and returns the data as promise
     return inquirer.prompt(questions)
-    
-    // use the inputs to run the generation function and spit out a new file
-    .then((answers) => {
-        const mdContent = writeToFile(mdData)
-        fs.writeFile('./README.md', mdContent, err => {
-        if (err) throw new Error(err);
-        console.log('readme created!')
-        })
-    })
-    
 }
 
+// initializes app
+function init() {
+    promptUser()
+    .then(mdData => {
+        console.log(mdData)
+        const mdContent = generateMarkdown(mdData)
+        fs.writeFile('./README.md', mdContent, err => {
+            if (err) throw new Error(err)
+            console.log('readme created!')
+        })
+    })
+}
 // Function call to initialize app
 init()
-
-    
-// uses data from mdContent to write file
-
-// const mdContent = generateMarkdown(mdData)
-// fs.writeFile('./README.md', mdContent, err => {
-//     if (err) throw new Error(err);
-//     console.log('readme created!');
-// })
 
 // sample question w/ error handling
 /*
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,31 +1,39 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
   if(!licenseBadge) {
     return ''
   }
 
-  return 'license badge goes here'
+  return `
+  [${license}]
+  `
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
+function renderLicenseLink(link) {
   if(!licenseLink) {
     return ''
   }
 
-  return 'license link goes here'
+  return `
+  ${link}
+  `
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
-module.exports = generateMarkdown => {
-  console.log()
-  return `# ${data.title}
+module.exports = mdData => {
+  console.log(mdData)
+  const { title, description, install, ...license } = mdData
+
+  return `
+  # ${mdData.title}
+
+  ${mdData.description}
 
 `;
 }
